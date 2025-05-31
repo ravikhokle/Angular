e8ee700ee7f3186a1badfe1fa61f929959acaf21
ravikhokle <ravikhokle1@gmail.com>
2025-05-31 08:23:00 +0530

    Custom Component

---
+++ custom-component/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ProfileComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'custom-component';
+}
